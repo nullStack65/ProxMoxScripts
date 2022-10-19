6004ddec2e61cbc0c22a568df5cfb4599f4cf0ef
nullStack65 <112618179+nullStack65@users.noreply.github.com>
2022-10-19 16:26:06 -0400

    Update ansibleInstall.sh

--- ansibleInstall.sh
+++ ansibleInstall.sh
@@ -1,6 +1,5 @@
 pveam update
 pveam download local ubuntu-22.04-standard_22.04-1_amd64.tar.gz
-
 pct create 100 local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst \
     --cores 1 --cpuunits 1024 \
     --memory 4096 --swap 0 \
@@ -12,18 +11,21 @@ pct create 100 local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst \
     --storage local-lvm \
     --password Chip086pit535hips$ \
     --onboot 1
-    
-pct exec 100 apt install python3-pip -y
-pct exec 100 pip install ansible
-pct exec 100 mkdir -p /etc/ansible/
-pct exec 100 cd /etc/ansible
-pct exec 100 touch hosts
-pct exec 100 echo "[myvirtualmachines]" >> hosts
-ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-pct exec 100 echo $ip4 >> hosts
-#ansible all --list-hosts
-pct exec 100 touch ansible.cfg
-pct exec 100 ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
+pct start 100
+pct enter 100
+apt install python3-pip -y
+pip install ansible
+mkdir -p /etc/ansible/
+touch ansible.cfg
+ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
+exit
+touch hosts
+echo "[myvirtualmachines]" >> hosts
+ip4=$(/sbin/ip -o -4 addr list vmbr0 | awk '{print $4}' | cut -d/ -f1)
+echo $ip4 >> hosts
 pct mount 100
+cp hosts /var/lib/lxc/100/rootfs/etc/ansible/hosts
 cp /var/lib/lxc/100/rootfs/root/.ssh/id_rsa.pub ~/.ssh/authorized_keys
-#ansible all -m ping
+pct unmount 100
+#pct exec 100 ansible all --list-hosts
+#pct exec 100 ansible all -m ping
